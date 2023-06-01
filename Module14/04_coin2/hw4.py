4b503f749f5ce946b26c34d74cdacc9f1fbdb36e
Николай Хмиров <nikolaykhmirov@gmail.com>
2023-06-01 21:38:07 +0300

    Upload New File

---
+++ Module14/04_coin2/hw4.py
@@ -0,0 +1,11 @@
+import math
+print('Введите координаты монетки:')
+x = float(input('x:'))
+y = float(input('y:'))
+radius = float(input('Введите радиус:'))
+if math.sqrt(x**2+y**2) <= radius:
+    print('Монетка где-то рядом')
+else:
+    print('Монетки в области нет')
+
+
