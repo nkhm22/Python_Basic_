7f99dcdc7b40a9a243a7d251e8af0a9f272d27e4
Николай Хмиров <nikolaykhmirov@gmail.com>
2023-06-02 21:24:17 +0300

    Upload New File

---
+++ Module15/02_tournament/hw2.py
@@ -0,0 +1,5 @@
+players = ['Артемий','Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+second_day=[]
+for number in range (1,8,2):
+ second_day.append(players[number])
+print('Второй день:',second_day)
