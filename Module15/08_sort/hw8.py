e8706c2a0c7180a9c65df8e8dd9ca6797e579adf
Николай Хмиров <nikolaykhmirov@gmail.com>
2023-06-04 13:24:05 +0300

    Upload New File

---
+++ Module15/08_sort/hw8.py
@@ -0,0 +1,15 @@
+def selection(list_digits_):
+  for i in range(len(list_digits_)):
+      for curr in range(i,len(list_digits_)):
+          if list_digits_[curr]<list_digits_[i]:
+              list_digits_[curr],list_digits_[i]=list_digits_[i],list_digits_[curr]
+
+N = int(input('Введите количество элементов списка:'))
+list_digits=[]
+for number in range(N):
+  digit = float(input('Введите элемент списка:'))
+  list_digits.append(digit)
+print(list_digits)
+selection(list_digits)
+print(list_digits)
+
