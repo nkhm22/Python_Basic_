dcc2367e4bd70bd18d6ef1a56e90f59a4208c5df
Николай Хмиров <nikolaykhmirov@gmail.com>
2023-06-07 20:22:33 +0300

    Upload New File

---
+++ Module16/02_line/hw2.py
@@ -0,0 +1,13 @@
+def selection(list_children):
+    for i in range(len(list_children)):
+        for curr in range(i, len(list_children)):
+            if list_children[curr] < list_children[i]:
+                list_children[curr], list_children[i] = list_children[i], list_children[curr]
+
+a=[]
+a.append(list(range (160,177,2)))
+b=[]
+b.append(list(range (162,181,3)))
+a.extend(b)
+selection(a)
+print(a)
