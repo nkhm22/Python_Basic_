75901c1e2ea3a48ede6a675b340998293350202e
Николай Хмиров <nikolaykhmirov@gmail.com>
2023-06-13 06:56:54 +0300

    Upload New File

---
+++ Module16/07_rhyme_cnt/hw7.py
@@ -0,0 +1,17 @@
+N = int(input('Количество человек:'))
+K = int(input('Какое число в считалке?'))
+print('Значит, выбывает каждый',K,'-й человек')
+
+people_list = list(range(1, N + 1))
+
+index_del = 0
+while len(people_list) > 1:
+    print('Текущий круг людей:', people_list)
+    index_start = index_del % len(people_list)
+    print('Начало счёта с номера', people_list[index_start])
+
+    index_del = (index_start + K - 1) % len(people_list)
+    print('Выбывает человек под номером', people_list[index_del])
+    people_list.remove(people_list[index_del])
+
+print('\nОстался человек под номером', people_list[0])
