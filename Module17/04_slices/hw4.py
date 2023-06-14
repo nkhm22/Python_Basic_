c843a9ffa87f81d178e47c63f95cf5a7e4abab9b
Николай Хмиров <nikolaykhmirov@gmail.com>
2023-06-14 21:12:50 +0300

    Upload New File

---
+++ Module17/04_slices/hw4.py
@@ -0,0 +1,12 @@
+alphabet = 'abcdefg'
+alphabet1=[letter for letter in alphabet]
+print(alphabet1)
+print(alphabet1[::-1])
+print(alphabet1[::2])
+print(alphabet1[1::2])
+print(alphabet1[1:])
+print(alphabet1[6:4:-1])
+print(alphabet1[2:3])
+print(alphabet1[4:7])
+print(alphabet1[2:4])
+print(alphabet1[3:1:-1])
