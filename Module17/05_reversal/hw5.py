033e8c34df95a3551ebf931299c0507c4dad83e8
Николай Хмиров <nikolaykhmirov@gmail.com>
2023-06-15 18:40:32 +0300

    Upload New File

---
+++ Module17/05_reversal/hw5.py
@@ -0,0 +1,5 @@
+text = input('Введите текст:')
+letter_h_1 = text.index('h')
+letter_h_2 = text.rindex('h')
+result = text[(letter_h_2-1):letter_h_1:-1]
+print('Развёрнутая последовательность между первым и последним h:',result)
