afbdd3536869569de00c6a9f3e56310781ac1196
Николай Хмиров <nikolaykhmirov@gmail.com>
2023-06-15 20:47:41 +0300

    Upload New File

---
+++ Module17/08_caesar_cipher/hw8.py
@@ -0,0 +1,12 @@
+def caesar_cipher(string,user_shift):
+    char_list=[(alphabet[(alphabet.index(sym)+user_shift)] % 33 if sym!= ' ' else ' ') for sym in string]
+    new_str= ' '
+    for i_char in char_list:
+        new_str+=i_char
+    return new_str
+
+alphabet='абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+text = input('Введите текст:')
+K = int(input('Введите сдвиг:'))
+
+output_str = caesar_cipher(text, K)
