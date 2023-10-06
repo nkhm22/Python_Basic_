9cdad042490509a57e0e2ac818fa039f1f76cffe
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-06 16:37:35 +0300

    Merge remote-tracking branch 'github/master'

--- Module20/01_code_review/main.py
+++ Module20/01_code_review/main.py
@@ -1,44 +1,39 @@
 students = {
-    1: {
-        'name': 'Bob',
-        'surname': 'Vazovski',
-        'age': 23,
-        'interests': ['biology, swimming']
-    },
-    2: {
-        'name': 'Rob',
-        'surname': 'Stepanov',
-        'age': 24,
-        'interests': ['math', 'computer games', 'running']
-    },
-    3: {
-        'name': 'Alexander',
-        'surname': 'Krug',
-        'age': 22,
-        'interests': ['languages', 'health food']
-    }
+1: {
+'name': 'Bob',
+'surname': 'Vazovski',
+'age': 23,
+'interests': ['biology, swimming']
+},
+2: {
+'name': 'Rob',
+'surname': 'Stepanov',
+'age': 24,
+'interests': ['math', 'computer games', 'running']
+},
+3: {
+'name': 'Alexander',
+'surname': 'Krug',
+'age': 22,
+'interests': ['languages', 'health food']
 }
-
-
-def f(dict):
-    lst = []
-    string = ''
-    for i in dict:
-        lst += (dict[i]['interests'])
-        string += dict[i]['surname']
-    cnt = 0
-    for s in string:
-        cnt += 1
-    return lst, cnt
-
-
-pairs = []
+}
+index=[]
+age=[]
+result_=[]
+result__=[]
+surnames=[]
 for i in students:
-    pairs += (i, students[i]['age'])
-
-
-my_lst = f(students)[0]
-l = f(students)[1]
-print(my_lst, l)
-
-# TODO исправить код
+    index.append(i)
+    result__.append(students[i]['interests'])
+    surnames.append(students[i]['surname'])
+result___=[interests for _ in result__ for interests in _]
+surnames_=[l_surname for __ in surnames for l_surname in __]
+print('Общая длина всех фамилий студентов:', len(surnames_))
+print('Полный список интересов всех студентов', result___)
+for a in students.keys():
+    age.append(students[i]['age'])
+result=zip(index,age)
+for res in result:
+    result_.append(res)
+print('Cписок пар «ID студента — возраст»:', result_)
