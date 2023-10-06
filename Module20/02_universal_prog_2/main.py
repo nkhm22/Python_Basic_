1d8128bd1f459b5c97a93abb876de0178510b6b5
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-06 16:47:12 +0300

    +++

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,28 @@
+text = input('Введите текст:')
+
+txt = []
+for letter in text:
+    txt.append(letter)
+
+
+def is_prime():
+    prime = []
+    for digit in range(2, len(txt)):
+        k = 0
+        for i in range(2, digit // 2 + 1):
+            if digit % i == 0:
+                k = k + 1
+        if (k <= 0):
+            prime.append(digit)
+    return prime
+
+
+def zip_sym():
+    res = []
+    for number in is_prime():
+        res.append(txt[number])
+    return res
+
+
+print(zip_sym())
 # TODO здесь писать код
