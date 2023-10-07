4d9dcb5c7cf788a97521b838a3b74e76749da38d
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-07 11:58:15 +0300

    +++

--- Module20/05_sort_function/main.py
+++ Module20/05_sort_function/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def tpl_sort(tpl_p):
+    for i in tpl_p:
+        if not isinstance(i, int):
+            return tpl_p
+    sorted_list=sorted(list(tpl_p))
+    sorted_tpl=tuple(sorted_list)
+    return sorted_tpl
+tpl = (6, 3.9, -1, 8, 4, 10, -5)
+print(tpl_sort(tpl))
--- Module20/07_my_zip/main.py
+++ Module20/07_my_zip/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+text='abcd'
+digits=(10, 20, 30, 40)
+t=[a for a in text]
+d=[b for b in digits]
+result=[]
+for c in zip(t,d):
+    result.append(c)
+print(result)
+print(zip(t,d))
