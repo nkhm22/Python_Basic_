fb3a9d7bae849cfea77303460457684bb2373005
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-12 18:38:36 +0300

    2

--- Module21/02_search_element_2/main.py
+++ Module21/02_search_element_2/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+def search_element(data, tag):
+    result = None
+    if tag in data:
+        return data[tag]
+    for _ in range(step-1):
+        for key, value in data.items():
+            if isinstance(value, dict):
+                result = search_element(value, tag)
+                if result:
+                    return result
+    return result
+
+
+site = {
+'html': {
+'head': {
+'title': 'Мой сайт'
+},
+'body': {
+'h2': 'Здесь будет мой заголовок',
+'div': 'Тут, наверное, какой-то блок',
+'p': 'А вот здесь новый абзац'
+}
+}
+}
+
+search_key = input("Искомый ключ: ")
+step=int(input('Введите максимальную глубину: '))
+value = search_element(site, search_key)
+if value:
+    print("Значение:", value)
+else:
+    print("Такого ключа в структуре сайта нет.")
+
