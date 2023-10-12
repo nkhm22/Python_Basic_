faad9b3355b3de59bd2b90928ddb76cee2de2789
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-12 18:36:34 +0300

    1

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def rec_func(num):
+    if num == 0:
+        return 1
+    else:
+        rec_func(num - 1)
+        return print(num)
+num = int(input('Введите num:'))
+rec_func(num)
