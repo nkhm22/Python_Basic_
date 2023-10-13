bed1c5b8cd68520402573a7b6b82d9c7f0ea48b2
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-13 12:29:40 +0300

    5

--- Module21/05_list_of_lists_2/main.py
+++ Module21/05_list_of_lists_2/main.py
@@ -3,7 +3,7 @@ list_question=[1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
 def sum_list(summ):
     if summ==[]:
         return summ
-    if isinstance(summ[0], list):
+    if isinstance(summ[0],list):
         return (sum_list(summ[0])+sum_list(summ[1:]))
-    return (summ[:1]+sum_list(summ[1:]))
+    return(summ[:1]+sum_list(summ[1:]))
 print(sum_list(list_question))
