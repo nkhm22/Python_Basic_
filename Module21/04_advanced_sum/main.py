a3274dc054a216ca460e6546f14094e4e30f802c
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-13 11:24:14 +0300

    4

--- Module21/04_advanced_sum/main.py
+++ Module21/04_advanced_sum/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+list_question=[[1, [2, [3]], [1]], 3]
+def sum_list(summ):
+    su=0
+    for elem in summ:
+        if isinstance(elem,int):
+            su+=elem
+        else:
+            su+=sum_list(elem)
+    return su
+print(sum_list(list_question))
