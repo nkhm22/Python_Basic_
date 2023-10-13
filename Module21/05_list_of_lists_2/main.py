5c0dbb7e791538bc925aca4152c1b9826809e700
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-13 12:20:43 +0300

    4

--- Module21/05_list_of_lists_2/main.py
+++ Module21/05_list_of_lists_2/main.py
@@ -1,4 +1,9 @@
-nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
+list_question=[1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
-
-# TODO здесь писать код
+def sum_list(summ):
+    if summ==[]:
+        return summ
+    if isinstance(summ[0], list):
+        return (sum_list(summ[0])+sum_list(summ[1:]))
+    return (summ[:1]+sum_list(summ[1:]))
+print(sum_list(list_question))
