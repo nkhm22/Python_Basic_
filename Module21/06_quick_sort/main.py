79af7c3293a96041013a5081b4f8217545ab74b2
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-13 13:47:03 +0300

    5

--- Module21/06_quick_sort/main.py
+++ Module21/06_quick_sort/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+list_hoar=[5, 8, 9, 4, 2, 9, 1, 8]
+def hoar(sequence):
+    big=[]
+    little=[]
+    middle=[]
+    if len(sequence)<=1:
+        return sequence
+    else:
+        for elem in sequence:
+            if elem>sequence[len(sequence)-1]:
+                big.append(elem)
+            if elem < sequence[len(sequence)-1]:
+                little.append(elem)
+            if elem == sequence[len(sequence)-1]:
+                middle.append(elem)
+        return hoar(little) + middle + hoar(big)
+print(hoar(list_hoar))
+
