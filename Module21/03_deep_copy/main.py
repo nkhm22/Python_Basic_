525a9fd00951a597760a769ad68f246e03409f51
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-14 14:02:40 +0300

    5

--- Module21/03_deep_copy/main.py
+++ Module21/03_deep_copy/main.py
@@ -1,3 +1,5 @@
+import copy
+
 site = {
     'html': {
         'head': {
@@ -10,5 +12,40 @@ site = {
         }
     }
 }
+def deep_copy(object):
+    if isinstance(object, (int, float, str)):
+        return object
+    elif isinstance(object, (list)):
+        return[deep_copy(item) for item in object]
+    elif isinstance(object, (dict)):
+        return {key: deep_copy(value) for key, value in object.items()}
+    else:
+        return copy.deepcopy(object)
 
-# TODO здесь писать код
+def find_key(strukture, key):
+    if key in strukture:
+        return strukture[key]
+    for substruct in strukture.values():
+        if isinstance (substruct,dict):
+            result = find_key(substruct,key)
+            print('0', result)
+            result = 'Куплю\продам {} недорого'.format(product)
+            substruct[key]=result
+            print('1', result)
+            if result:
+                break
+    else:
+        result=None
+    return strukture
+
+sites=int(input('Сколько сайтов?'))
+count=1
+site_list=[]
+while count<=sites:
+    product=input('Введите название продукта для нового сайта: ')
+    productes_sait=deep_copy(site)
+    print('Копия:')
+    count+=1
+    value=find_key(site, 'title')
+    if value:
+        print('Сайт для {}:'.format(product), 'site', value)
