bae9434e7ea3b215c84a9a7607f1dc48cac40201
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-18 22:08:39 +0300

    1

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import os
+
+open_file=open('numbers.txt','r')
+num_sum=0
+for line in open_file:
+    if line.rstrip():
+        num_sum+=int(line.rstrip())
+open_file.close()
+file_in = open("answer.txt", "w")
+file_in.write(str(num_sum))
+file_in.close()
