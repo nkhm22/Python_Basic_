a403db4bfd78a35cea5814d46063a83bda48afdf
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-23 00:02:51 +0300

    4

--- Module22/04_tournament/main.py
+++ Module22/04_tournament/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+tour=open('first_tour.txt','r')
+name=[]
+list_2_tour=[]
+for string in tour:
+    name.append(string)
+for elem in name:
+    for ielem in elem.split():
+        if ielem.isnumeric():
+            if int(ielem)>int(name[0]):
+                list_2_tour.append(elem)
+second_tour = open('second_tour.txt', 'w')
+second_tour.write(str(len(list_2_tour)))
+second_tour.write('\n')
+for i_elem in list_2_tour:
+    second_tour.write(i_elem.split()[1][0])
+    second_tour.write('. ')
+    second_tour.write(i_elem.split()[0])
+    second_tour.write('\t')
+    second_tour.write(i_elem.split()[2])
+    second_tour.write('\n')
+
