ea17c126cb4f4038039a5f7a74bc3dea9a50036b
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-22 18:09:40 +0300

    2

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import os
+
+zen_file=open('zen.txt','r')
+count=0
+zen_list=[]
+for string in zen_file:
+    zen_list.append(string)
+conversely_file=open('conversely.txt','w')
+for conversely_string in zen_list[::-1]:
+    conversely_file.write(conversely_string)
+conversely_file.close()
