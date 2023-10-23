5102f47b16e2a4547765dea1064f0c6becf603fc
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-23 22:18:09 +0300

    3

--- Module22/03_files_and_folders/main.py
+++ Module22/03_files_and_folders/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+import os
+path='C:\\Users\\79067\\OneDrive\\Рабочий стол\\Задания Skillbox\\Python_Basic\\Module16'
+def find_file(cur_path):
+    files=0
+    dir=0
+    size=0
+    for elem in os.listdir(cur_path):
+        path1=os.path.abspath(elem)
+        if os.path.isfile(elem):
+            files+=1
+            size+=os.path.getsize(elem)
+        else:
+            dir+=1
+            find_file(path1)
+    return files, dir, size
+print(find_file(path))
