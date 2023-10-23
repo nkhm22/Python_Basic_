69a2ac2832d4308f9abeb9886a557f52027efbf5
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-23 21:05:41 +0300

    4

--- Module22/05_frequency_analysis/main.py
+++ Module22/05_frequency_analysis/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+text=open('text.txt', 'r')
+analysis=open('analysis.txt', 'w')
+frequency = {}
+for line in text:
+    for symbol in line:
+        if symbol!=' ':
+            if symbol in frequency:
+                frequency[symbol] += 1
+            else:
+                frequency[symbol] = 1
+
+    for letter, freq in frequency.items():
+        print(letter, ':', round(float(freq/len(line)), 3))
+        analysis.write(letter)
+        analysis.write(' : ')
+        analysis.write(str(round(float(freq/len(line)), 3)))
+        analysis.write('\n')
