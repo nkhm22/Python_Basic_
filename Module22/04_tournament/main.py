80f173eac6a0120e7a027e9cb16731f275c32f17
Alexey <alexei@bk.ru>
2023-10-24 20:51:27 +0300

    sort

--- Module22/04_tournament/main.py
+++ Module22/04_tournament/main.py
@@ -1,17 +1,20 @@
-tour=open('first_tour.txt','r')
-name=[]
-list_2_tour=[]
+tour = open('first_tour.txt', 'r')
+name = []
+list_2_tour = []
 for string in tour:
     name.append(string)
 for elem in name:
     for ielem in elem.split():
         if ielem.isnumeric():
-            if int(ielem)>int(name[0]):
-                list_2_tour.append(elem)
+            if int(ielem) > int(name[0]):
+                list_2_tour.append((elem, int(ielem)))
 second_tour = open('second_tour.txt', 'w')
 second_tour.write(str(len(list_2_tour)))
 second_tour.write('\n')
-for i_elem in list_2_tour:
+
+list_2_tour.sort(key=lambda x: x[1], reverse=True)
+
+for i_elem, _ in list_2_tour:
     second_tour.write(i_elem.split()[1][0])
     second_tour.write('. ')
     second_tour.write(i_elem.split()[0])
