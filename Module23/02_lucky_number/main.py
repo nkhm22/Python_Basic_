f6d7397960453920f5031902827391884848bd76
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-29 12:28:40 +0300

    2

--- Module23/02_lucky_number/main.py
+++ Module23/02_lucky_number/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+import random
+digits=open('out_file.txt', 'w', encoding='utf-8')
+count = 0
+while True:
+    digit = int(input('Введите число:'))
+    if digit == random.sample(range(1, 777), int(777/14)):
+        print('Вас постигла неудача!', file=digits)
+        break
+    count += digit
+    digits.write(str(digit))
+    digits.write('\n')
+    if count >= 777:
+        print('Вы успешно выполнили условие для выхода из порочного цикла!')
+        break
+digits.close()
