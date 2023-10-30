f6cefd8c5b9ff47c2eb580499219377eebb92a93
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-30 20:08:05 +0300

    5

--- Module23/05_reliable_calculations/main.py
+++ Module23/05_reliable_calculations/main.py
@@ -1,10 +1,18 @@
-# TODO здесь писать код
-
-# Здесь создайте функцию get_sage_sqrt
-
-
-# Тестовые случаи
+import math
+def get_sage_sqrt(digit):
+    try:
+        if isinstance(digit, str):
+            raise TypeError('Элемент не является числом')
+        if digit <= 0:
+            raise ValueError('Нельзя извлечь квадратный корень из отрицательного числа и 0')
+        res = math.sqrt(digit)
+        return res
+    except TypeError as exc:
+        print(exc)
+    except Exception as exc:
+        print(exc)
 numbers = [16, 25, -9, 0, 4.5, "abc"]
 for number in numbers:
     result = get_sage_sqrt(number)
-    print(f"Квадратный корень numbers {number}: {result}")
+    if isinstance(result, float) or isinstance(result, int):
+        print(f"Квадратный корень numbers {number}: {result}")
