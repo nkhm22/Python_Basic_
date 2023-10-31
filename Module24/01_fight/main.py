e7281ed66e69f05138210ae4f45b214a52037718
nkhm22 <nikolaykhmirov@gmail.com>
2023-10-31 20:31:11 +0300

    1

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+import random
+class Unit():
+    number = ''
+    points = 100
+
+player_1 = Unit()
+player_2 = Unit()
+player_1.number = '1'
+player_2.number = '2'
+for _ in range(11):
+    command = random.randint(1, 2)
+    if command == 1:
+        player_2.points -= 20
+        print('Атаковал игрок 1, у игрока 2 осталось {} здоровья'.format(player_2.points))
+        if player_2.points <= 0:
+            print('Победил игрок 1')
+            break
+    if command == 2:
+        player_1.points -= 20
+        print('Атаковал игрок 2, у игрока 1 осталось {} здоровья'.format(player_1.points))
+        if player_1.points <= 0:
+            print('Победил игрок 2')
+            break
