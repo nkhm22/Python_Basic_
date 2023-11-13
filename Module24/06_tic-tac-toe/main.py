666f00315dc9971acfba0dc0ff5b21e29f8a804b
nkhm22 <nikolaykhmirov@gmail.com>
2023-11-13 21:20:18 +0300

    Не понимаю, почему пишет, что индекс символа вне диапазона

--- Module24/06_tic-tac-toe/main.py
+++ Module24/06_tic-tac-toe/main.py
@@ -1,1 +1,61 @@
-# TODO здесь писать код
+class Cell:
+    def __init__(self, number, busy = False):
+        self.number = str(number)
+        self.busy = busy
+class Board:
+    def __init__(self):
+        self.board = [Cell(i) for i in range(1, 10)]
+    def draw_board(self):
+        print('+' * 13)
+        for i in range(3):
+            print('|', self.board[i * 3].number, '|', self.board[1 + i * 3].number, '|', self.board[2 + i * 3].number, '|')
+            print('+' * 13)
+    def check_win(self):
+        combination_win = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
+        for comb in combination_win:
+            if player_1.symb[comb[0]] == player_1.symb[comb[1]] == player_1.symb[comb[2]]:
+                if player_1.symb == self.board[comb[0]].number:
+                    print('Победил {}'.format(player_1.name))
+                else:
+                    print('Победил {}'.format(player_2.name))
+                return True
+            return False
+    def check_busy(self, number):
+        if self.board[number - 1].busy:
+            return True
+        return False
+class Player:
+    def __init__(self, name, symb, win = False):
+        self.name = name
+        self.symb = symb
+        self.win = win
+    def go_cell(self, number, Board):
+        Board.board[number - 1].busy = False
+        Board.board[number - 1].number = self.symb
+class Game:
+    def start(self, player, board):
+        number = int(input('Куда ходит, {}?'.format(player.name)))
+        if board.check_busy(number):
+            print('Клетка занята')
+            Game.start(self, player, board)
+        else:
+            player.go_cell(number, board)
+player_1 = Player('Игрок 1', 'X')
+player_2 = Player('Игрок 2', 'O')
+board = Board()
+game = Game()
+count = 0
+while True:
+    board.draw_board()
+    if count % 2 == 0:
+        game.start(player_1, board)
+        if board.check_win():
+            break
+    else:
+        game.start(player_2, board)
+        if board.check_win():
+            break
+    count += 1
+    if count == 9:
+        print('Ничья')
+        break
