d917555f121c44d1a5ad877630b2f6280b387e9e
nkhm22 <nikolaykhmirov@gmail.com>
2023-11-15 20:11:20 +0300

    7

--- Module24/07_Matrix/main.py
+++ Module24/07_Matrix/main.py
@@ -1,8 +1,42 @@
-# TODO здесь писать код
-
-
-
-
+class Matrix:
+    def __init__(self, line, column):
+        self.line = line
+        self.column = column
+        self.data = []
+    def __str__(self):
+        res = ''
+        for elem in self.data:
+            res += '\t'.join(str(el) for el in elem)
+            res += '\n'
+        return res
+    def add(self, new_matrix):
+        res = [[self.data[l][c] + new_matrix.data[l][c] for c in range(self.column)] for l in range(self.line)]
+        result = ''
+        for elem in res:
+            result += '\t'.join(str(el) for el in elem)
+            result += '\n'
+        return result
+    def subtract(self, new_matrix):
+        res = [[self.data[l][c] - new_matrix.data[l][c] for c in range(self.column)] for l in range(self.line)]
+        result = ''
+        for elem in res:
+            result += '\t'.join(str(el) for el in elem)
+            result += '\n'
+        return result
+    def multiply(self, new_matrix):
+        res = [[sum(a * b for a, b in zip(A_line, B_column)) for B_column in zip(*new_matrix.data)] for A_line in self.data]
+        result = ''
+        for elem in res:
+            result += '\t'.join(str(el) for el in elem)
+            result += '\n'
+        return result
+    def transpose(self):
+        res = [[row[i] for row in self.data] for i in range(self.column)]
+        result = ''
+        for elem in res:
+            result += '\t'.join(str(el) for el in elem)
+            result += '\n'
+        return result
 # Примеры работы с классом:
 
 # Создание экземпляров класса Matrix
