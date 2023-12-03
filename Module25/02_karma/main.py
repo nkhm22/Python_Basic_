2c28bf558c61b3cd748f6b70de75854020fec5cd
nkhm22 <nikolaykhmirov@gmail.com>
2023-12-03 12:54:52 +0300

    Почему-то всегда за весь цикл одно исключение. Пытался через классы сделать, выдает ошибки

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,20 +1,18 @@
 import random
-class Exception:
-    def __init__(self):
-        self.list_error = [KillError, DrunkError, CarCrashError, GluttonyError, DepressionError]
 
 
-class KillError(Exception):
+
+class KillError():
     def __str__(self):
         return 'Убил человека'
 
 
-class DrunkError(Exception):
+class DrunkError():
     def __str__(self):
         return'Напился'
 
 
-class CarCrashError(Exception):
+class CarCrashError():
     def __str__(self):
         return 'Попал в ДТП'
 
@@ -24,21 +22,23 @@ class GluttonyError(Exception):
         return 'Объелся'
 
 
-class DepressionError(Exception):
+class DepressionError():
     def __str__(self):
         return 'Впал в депрессию'
 
 
 def one_day():
-    file = open('karma.log','w')
     if random.randint(1, 10) != 10:
         return random.randint(1, 7)
     else:
-        return 0
-        file.write(random.choice(Exception.list_error))
+        raise BaseException
 
 
 karma = 0
 while karma <= 500:
-    karma += one_day()
-    print(karma)
+    try:
+        karma += one_day()
+        print(karma)
+    except BaseException:
+        with open('karma.log', 'w', encoding='utf-8') as errors:
+            errors.write(random.choice(['Убил человека', 'Напился', 'Попал в ДТП', 'Объелся', 'Впал в депрессию']))
