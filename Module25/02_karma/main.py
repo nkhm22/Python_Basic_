78c2bc8b4ce7156c25f19d76287f47c2c36d6fcd
nkhm22 <nikolaykhmirov@gmail.com>
2023-12-03 11:28:10 +0300

    2

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,1 +1,44 @@
-# TODO здесь писать код
+import random
+class Exception:
+    def __init__(self):
+        self.list_error = [KillError, DrunkError, CarCrashError, GluttonyError, DepressionError]
+
+
+class KillError(Exception):
+    def __str__(self):
+        return 'Убил человека'
+
+
+class DrunkError(Exception):
+    def __str__(self):
+        return'Напился'
+
+
+class CarCrashError(Exception):
+    def __str__(self):
+        return 'Попал в ДТП'
+
+
+class GluttonyError(Exception):
+    def __str__(self):
+        return 'Объелся'
+
+
+class DepressionError(Exception):
+    def __str__(self):
+        return 'Впал в депрессию'
+
+
+def one_day():
+    file = open('karma.log','w')
+    if random.randint(1, 10) != 10:
+        return random.randint(1, 7)
+    else:
+        return 0
+        file.write(random.choice(Exception.list_error))
+
+
+karma = 0
+while karma <= 500:
+    karma += one_day()
+    print(karma)
