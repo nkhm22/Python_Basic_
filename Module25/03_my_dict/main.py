2585f7a40c93b0accdd789c17caf004894704393
nkhm22 <nikolaykhmirov@gmail.com>
2024-01-14 13:06:25 +0300

    3

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+class Mydict(dict):
+    def get(self, key, default = 0):
+        return super().get(key, default)
+
+def main():
+    print('Проверки')
+    my_dict = Mydict()
+    my_dict['a'] = 1
+    my_dict['b'] = 2
+    print('Ключ a', my_dict['a'])
+    print('Ключ b', my_dict['b'])
+    print('Несуществующий ключ:', my_dict.get('c'))
+
+main()
