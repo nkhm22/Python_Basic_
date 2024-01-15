b530ddada6f445f915d13a01e6296a37b52ce550
nkhm22 <nikolaykhmirov@gmail.com>
2024-01-15 20:32:37 +0300

    Модуль 14

--- Module14/04_coin2/main.py
+++ Module14/04_coin2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import math
+print('Введите координаты монетки:')
+x = float(input('x:'))
+y = float(input('y:'))
+radius = float(input('Введите радиус:'))
+if math.sqrt(x**2+y**2) <= radius:
+    print('Монетка где-то рядом')
+else:
+    print('Монетки в области нет')
