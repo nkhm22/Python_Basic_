daa4865fb32ee7ae4645827842f7d30645e8a781
nkhm22 <nikolaykhmirov@gmail.com>
2024-01-28 10:44:23 +0300

    4

--- Module25/04_RPG_game/heroes.py
+++ Module25/04_RPG_game/heroes.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Hero:
     # Базовый класс, который не подлежит изменению
     # У каждого наследника будут атрибуты:
@@ -42,12 +45,13 @@ class Hero:
     # Метод make_a_move базового класса могут вызывать только герои, не монстры.
     def attack(self, target):
         # Каждый наследник будет наносить урон согласно правилам своего класса
-        #raise NotImplementedError("Вы забыли переопределить метод Attack!")
         pass
+
     def take_damage(self, damage):
         # Каждый наследник будет получать урон согласно правилам своего класса
         # При этом у всех наследников есть общая логика, которая определяет жив ли объект.
-        print("\t", self.name, "Получил удар с силой равной = ", round(damage), ". Осталось здоровья - ", round(self.get_hp()))
+        print("\t", self.name, "Получил удар с силой равной = ", round(damage), ". Осталось здоровья - ",
+              round(self.get_hp()))
         # Дополнительные принты помогут вам внимательнее следить за боем и изменять стратегию, чтобы улучшить выживаемость героев
         if self.get_hp() <= 0:
             self.__is_alive = False
@@ -57,10 +61,11 @@ class Hero:
         self.set_power(self.get_power() + 0.1)
 
     def __str__(self):
-        return f'{self.name}, {self.get_hp()}, HP'
+        return 'Name: {0} | HP: {1}'.format(self.name, self.get_hp())
 
 
 class Healer(Hero):
+
     def __init__(self, name):
         super().__init__(name)
         self.magic_power = self.get_power() * 3
@@ -81,33 +86,33 @@ class Healer(Hero):
         target_of_potion = friends[0]
         min_health = target_of_potion.get_hp()
         for friend in friends:
-            if friend.get_hp() == min_health:
+            if friend.get_hp() < min_health:
                 target_of_potion = friend
                 min_health = target_of_potion.get_hp()
-        if min_health <= 100:
-            print('Исцеляю:', target_of_potion.name)
+
+        if min_health <= random.randint(60, 100):
+            print("Исцеляю", target_of_potion.name)
             self.hill(target_of_potion)
         else:
             if not enemies:
                 return
-            print('Атакую ближнего:', enemies[0])
+            print("Атакую ближнего -", enemies[0].name)
             self.attack(enemies[0])
         print('\n')
 
-    def __str__(self):
-        return super().__str__()
-     # Целитель:
-    # Атрибуты:
-    # - магическая сила - равна значению НАЧАЛЬНОГО показателя силы умноженному на 3 (self.__power * 3)
-    # Методы:
-    # - атака - может атаковать врага, но атакует только в половину силы self.__power
-    # - получение урона - т.к. защита целителя слаба - он получает на 20% больше урона (1.2 * damage)
-    # - исцеление - увеличивает здоровье цели на величину равную своей магической силе
-    # - выбор действия - получает на вход всех союзников и всех врагов и на основе своей стратегии выполняет ОДНО из действий (атака,
-    # исцеление) на выбранную им цель
+
+# Атрибуты:
+# - магическая сила - равна значению НАЧАЛЬНОГО показателя силы умноженному на 3 (self.__power * 3)
+# Методы:
+# - атака - может атаковать врага, но атакует только в половину силы self.__power
+# - получение урона - т.к. защита целителя слаба - он получает на 20% больше урона (1.2 * damage)
+# - исцеление - увеличивает здоровье цели на величину равную своей магической силе
+# - выбор действия - получает на вход всех союзников и всех врагов и на основе своей стратегии выполняет ОДНО из действий (атака,
+# исцеление) на выбранную им цель
 
 
 class Tank(Hero):
+
     def __init__(self, name):
         super().__init__(name)
         self.defense = 1
@@ -133,40 +138,45 @@ class Tank(Hero):
     def make_a_move(self, friends, enemies):
         super().make_a_move(friends, enemies)
         print(self.name, end=' ')
-        if self.get_hp() < 90 and not self.shield:
+
+        if self.get_hp() < 80 and not self.shield:
             self.set_shield_state()
-        elif self.get_hp() > 90 and self.shield:
+        elif self.get_hp() > 80 and self.shield:
             self.set_shield_state()
         else:
             target = enemies[0]
             min_health = target.get_hp()
+
             for enemy in enemies:
                 if enemy.get_hp() < min_health:
                     target = enemy
                     min_health = enemy.get_hp()
-            print('Атакую ближнего -', target.name)
+
+            print("Атакую ближнего -", target.name)
             self.attack(target)
             print('\n')
 
     def __str__(self):
-        return super().__str__()
-    # Танк:
-    # Атрибуты:
-    # - показатель защиты - изначально равен 1, может увеличиваться и уменьшаться
-    # - поднят ли щит - танк может поднимать щит, этот атрибут должен показывать поднят ли щит в данный момент
-    # Методы:
-    # - атака - атакует, но т.к. доспехи очень тяжелые - наносит половину урона (self.__power)
-    # - получение урона - весь входящий урон делится на показатель защиты (damage/self.defense) и только потом отнимается от здоровья
-    # - поднять щит - если щит не поднят - поднимает щит. Это увеличивает показатель брони в 2 раза, но уменьшает показатель силы в 2 раза.
-    # - опустить щит - если щит поднят - опускает щит. Это уменьшает показатель брони в 2 раза, но увеличивает показатель силы в 2 раза.
-    # - выбор действия - получает на вход всех союзников и всех врагов и на основе своей стратегии выполняет ОДНО из действий (атака,
-    # поднять щит/опустить щит) на выбранную им цель
+        return super().__str__() + f" | {self.shield}"
+
+# Танк:
+# Атрибуты:
+# - показатель защиты - изначально равен 1, может увеличиваться и уменьшаться
+# - поднят ли щит - танк может поднимать щит, этот атрибут должен показывать поднят ли щит в данный момент
+# Методы:
+# - атака - атакует, но т.к. доспехи очень тяжелые - наносит половину урона (self.__power)
+# - получение урона - весь входящий урон делится на показатель защиты (damage/self.defense) и только потом отнимается от здоровья
+# - поднять щит - если щит не поднят - поднимает щит. Это увеличивает показатель брони в 2 раза, но уменьшает показатель силы в 2 раза.
+# - опустить щит - если щит поднят - опускает щит. Это уменьшает показатель брони в 2 раза, но увеличивает показатель силы в 2 раза.
+# - выбор действия - получает на вход всех союзников и всех врагов и на основе своей стратегии выполняет ОДНО из действий (атака,
+# поднять щит/опустить щит) на выбранную им цель
 
 
 class Attacker(Hero):
+
     def __init__(self, name):
         super().__init__(name)
-        self.power_multiply = 3
+        self.power_multiply = 1
 
     def attack(self, target):
         target.take_damage(self.get_power() * self.power_multiply)
@@ -185,22 +195,22 @@ class Attacker(Hero):
     def make_a_move(self, friends, enemies):
         super().make_a_move(friends, enemies)
         print(self.name, end=' ')
+
         if self.power_multiply < 4:
             self.power_up()
             return
+
         target = enemies[0]
         min_health = target.get_hp()
+
         for enemy in enemies:
             if enemy.get_hp() < min_health:
                 target = enemy
                 min_health = enemy.get_hp()
-        print('Атакую ближнего -', target.name)
+
+        print("Атакую ближнего -", target.name)
         self.attack(target)
         print('\n')
-
-    def __str__(self):
-        return super().__str__()
-
     # Убийца:
     # Атрибуты:
     # - коэффициент усиления урона (входящего и исходящего)
